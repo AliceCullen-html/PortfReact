da9de5d9ff4a0c7beb239907a2bb2f1733eef741
AliceCullen-html <marcusvcunha0800@hotmail.com>
2022-12-06 00:33:37 -0300

    Second commit

--- port_react/src/App.jsx
+++ port_react/src/App.jsx
@@ -2,6 +2,7 @@
 import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
 import "./style/components/app.sass";
+import "./style/components/app.sass";
 
 
 function App() {
--- port_react/src/components/Sidebar.jsx
+++ port_react/src/components/Sidebar.jsx
@@ -1,8 +1,17 @@
+import Avatar from '../img/eu.jpg';
+
 
 const Sidebar = () => {
-    return (
-        <div>Sidebar</div>
-    )
+    return <aside id="sidebar">
+        <p>imagem</p>
+        <p className="title"> Desenvolvedor</p>
+        <p>Redes Sociais</p>
+        <p>informações</p>
+        <a href="" className="btn">
+            Download currículo
+        </a>
+
+    </aside>;
 }
 
 export default Sidebar
